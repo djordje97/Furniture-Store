d0c0b14091f8e344552b29e172daaa7523d909d9
Djordje Stevanovic <djordje97.tkd@gmail.com>
2017-10-16 12:27:59 +0200

    Init commit

---
+++ PoP/Test/Test1.cs
@@ -0,0 +1,24 @@
+﻿using Prostorimena;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoP.Test
+{
+    class Test1
+    {
+        public Test1()
+        {
+
+            A a = new A();
+            a.Setime("Naziv svih naziva");
+            Console.WriteLine("Naziv je " + a.Getime());
+
+            a.Ime = "ime svih imena";
+            Console.WriteLine("Ime je " + a.Ime);
+            Console.ReadLine();
+        }
+    }
+}
