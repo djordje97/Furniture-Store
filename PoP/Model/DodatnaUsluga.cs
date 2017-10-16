142e9107f190ea34e07ba07ac160473ca39d9f5c
Djordje Stevanovic <djordje97.tkd@gmail.com>
2017-10-16 13:24:52 +0200

    Konstruisan model aplikacije

---
+++ PoP/Model/DodatnaUsluga.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoP.Model
+{
+   
+   public class DodatnaUsluga
+    {
+        public int ID { get; set; }
+        public bool Obrisan { get; set; }
+        public string NazivUsluge { get; set; }
+        public double CenaUsluge { get; set; }
+       
+
+    }
+}
---
+++ PoP/Model/ProdajaNamestaja.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoP.Model
+{
+   public class ProdajaNamestaja
+    {
+        public int Id { get; set; }
+        public List<Namestaj> NamestajZaProdaju { get; set; }
+        public DateTime DatumProdaje { get; set; }
+        public int BrojRacuna { get; set; }
+        public String Kupac { get; set; }
+        public double PDV { get; set; }
+        public List<DodatnaUsluga> DodatneUsluge { get; set; }
+        public double UkupanIznos { get; set; }
+
+    }
+}
